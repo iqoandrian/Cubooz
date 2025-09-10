dc8044c558eb7db3adf5ef0f3c829836d5eb20ba
Iqo <iqoandrian@gmail.com>
2025-09-10 11:45:48 +0700

    hapus console log

--- js/main.js
+++ js/main.js
@@ -12,5 +12,3 @@ document.getElementById('login-btn').addEventListener('click', async () => {
   startGame();
   loadLeaderboard();
 });
-console.log("Scene:", scene);
-console.log("CubeGroup:", cubeGroup.children.length);
